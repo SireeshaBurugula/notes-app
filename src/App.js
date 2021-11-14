84c9856abfcbd139899ade685b1f92bdd74a0654
SireeshaBurugula <68294536+SireeshaBurugula@users.noreply.github.com>
2021-11-15 00:01:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import { nanoid } from 'nanoid';
+import  {useState} from 'react';
+const App =() => {
+  const [notes,setNotes] = useState([
+    {
+       id:nanoid(),
+       text:'first note',
+    },
+    {
+      
+      id:nanoid(),
+      text:'second note',
+    },
+    {
+      
+      id:nanoid(),
+      text:'third note',
+    },
+    {
+      
+      id:nanoid(),
+      text:'fourth note',
+    },
+    {
+      
+      id:nanoid(),
+      text:'fifth note',
+    },
+    {
+      
+      id:nanoid(),
+      text:'sixth note',
+    }
+  ]);
+  return (
+    <div className="container">
+      <Search/>
+      <NotesList notes={notes}/>
+    </div>
+  );
+}
+
+export default App;
