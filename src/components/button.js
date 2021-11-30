4609ab527f719940d5f506583a6795c93f62c83d
Tarunssd <tarunssd2000@gmail.com>
2021-11-30 21:39:50 +0530

    Added button transition

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { useState } from 'react';
 import { MdAddCircle } from "react-icons/md";
+import { IoIosRadioButtonOn } from "react-icons/io";
 
 const PlusButton = () =>{
+    const [colors,setcolors]= useState(false);
+    const EventFunction = () =>{
+        setcolors(true)
+        ActivateColors()
+    }
+
+    const ActivateColors = () =>{
+        if(colors === false){
+            render(
+                <div class="Colors">
+                    <IoIosRadioButtonOn color="orange" class="colorpick"/>
+                    <IoIosRadioButtonOn color="green" class="colorpick"/>
+                    <IoIosRadioButtonOn color="blue" class="colorpick"/>
+                    <IoIosRadioButtonOn color="yellow" class="colorpick"/>
+                </div>
+            )
+        }
+        else{
+            render()
+        }
+    }
     return(
         <div class="ButtonCont">
-            <MdAddCircle class="Button" size={50} color="#2687de"/>
+            <button class="Button" onClick={() => EventFunction()}><MdAddCircle size={50} color="#2687de" /></button>
         </div>
     )
 }
 
 export default PlusButton;
+
