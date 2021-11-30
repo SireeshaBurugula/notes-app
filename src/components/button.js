bce94ec56f44962a21b274a20f0326507ab2733a
Tarunssd <tarunssd2000@gmail.com>
2021-11-30 22:56:14 +0530

    Transition(Button)
    Made transition a bit smooth

--- src/components/button.js
+++ src/components/button.js
@@ -6,24 +6,31 @@ import { IoIosRadioButtonOn } from "react-icons/io";
 const PlusButton = () =>{
     const [colors,setcolors]= useState(false);
     const EventFunction = () =>{
-        setcolors(true)
         ActivateColors()
+        setcolors(true)
     }
 
     const ActivateColors = () =>{
         if(colors === false){
             render(
                 <div class="Colors">
-                    <IoIosRadioButtonOn color="orange" class="colorpick"/>
-                    <IoIosRadioButtonOn color="green" class="colorpick"/>
-                    <IoIosRadioButtonOn color="blue" class="colorpick"/>
-                    <IoIosRadioButtonOn color="yellow" class="colorpick"/>
+                    <IoIosRadioButtonOn color="orange" class="colorpick" id="topToBottom"/>
+                    <IoIosRadioButtonOn color="green" class="colorpick" id="topToBottom"/>
+                    <IoIosRadioButtonOn color="blue" class="colorpick" id="topToBottom"/>
+                    <IoIosRadioButtonOn color="yellow" class="colorpick" id="topToBottom"/>
                 </div>
             )
         }
-        else{
-            render()
-        }
+        // else{
+        //     render(
+        //         <div class="Colors">
+        //             <IoIosRadioButtonOn color="orange" class="colorpick" id="bottomtotop"/>
+        //             <IoIosRadioButtonOn color="green" class="colorpick" id="bottomtotop"/>
+        //             <IoIosRadioButtonOn color="blue" class="colorpick" id="bottomtotop"/>
+        //             <IoIosRadioButtonOn color="yellow" class="colorpick" id="bottomtotop"/>
+        //         </div>
+        //     )
+        // }
     }
     return(
         <div class="ButtonCont">
